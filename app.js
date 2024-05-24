4df0a74913e0f6a69ba0b6c3a0b2dd1c5f8fb332
Taco Hsu <chaoyu.hsuu@gmail.com>
2024-05-24 11:48:53 +0800

    modify app.js

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const exphbs = require('express-handlebars')
+const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.use('/public', express.static('public'));
-// app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'public')));  // public 文件夾用於靜態資源
+// app.use(express.static(path.join(__dirname, 'build')));  // build 文件夾用於 React 構建輸出
 
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.get('/', (req, res) => {
-  const homePageImage = 'public/images/home_page_image.png';
+  const homePageImage = '/images/home_page_image.png';
   res.render('index', { homePageImage: homePageImage });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(port, () => {
